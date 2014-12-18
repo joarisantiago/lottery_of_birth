d1cb905cd6ecc61d57ebc4a22e55241d3ddcc984
Sung Won Cho <mikeswcho@gmail.com>
2014-12-18 18:12:04 +1100

    Select random countries in MainCtrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,13 +10,26 @@
 angular.module('lotteryOfBirthApp')
   .controller('MainCtrl', ['$scope', 'randomizeBirth', function ($scope, randomizeBirth) {
 
+  	$scope.colorSelected = '#0FA0FA';
+  	$scope.colorDefault = '#ABDDA4';
+
     $scope.mapInit = function() { 
     	this.map = new Datamap({ element: document.getElementById('map') });
     };
 
     $scope.birth = function() {
-    	this.map.updateChoropleth({ 
-    		CAN: '#0fa0fa'
-    	});
+    	var country = randomizeBirth.get();
+
+    	this.updateMap(country);
+    };
+
+    $scope.updateMap = function(country) { 
+    	var data = {};
+    	data[country] = this.colorSelected;
+
+    	// Unhighlight previously selected countries
+    	$(".datamaps-subunit").css('fill', this.colorDefault);
+
+    	this.map.updateChoropleth(data);
     };
   }]);
--- app/scripts/services/randomizeBirth.js
+++ app/scripts/services/randomizeBirth.js
@@ -4,6 +4,12 @@ angular.module('lotteryOfBirthApp')
   .service('randomizeBirth', function() {
   	this.countryList = getCountryList();
   	this.numCountries = getCountryList().length;
+
+  	this.get = function () { 
+  		var countries = this.countryList;
+  		var rand = countries[Math.floor(Math.random() * countries.length)];
+  		return rand;
+  	};
   });
 
 var getCountryList = function() {
@@ -13,4 +19,4 @@ var getCountryList = function() {
 	var countryList = _.without(names, '-99');
 	
 	return countryList;
-}
+};
